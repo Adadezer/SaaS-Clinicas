d6340e0be4a0e38e17d65fb9d6845d933086ef5a
adadezer <adadezer@gmail.com>
2025-06-08 13:39:36 -0300

    feat: implement authentication and redirection in the dashboard

--- doutor-agenda/src/app/dashboard/page.tsx
+++ doutor-agenda/src/app/dashboard/page.tsx
@@ -1,9 +1,26 @@
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 import React from "react";
 
-function DashboardPage() {
+import { auth } from "@/lib/auth";
+
+import SignOutButtom from "./componets/sign-out-buttom";
+
+async function DashboardPage() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session?.user) {
+    redirect("/authentication");
+  }
+
   return (
     <div>
-      <h1>Dashboard page</h1>
+      <h1>Dashboard</h1>
+      <h1>{session?.user?.name}</h1>
+      <h1>{session?.user?.email}</h1>
+      <SignOutButtom />
     </div>
   );
 }
