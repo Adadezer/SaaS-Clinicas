2274515c87a0ed3af316a9b2575d1116fcd4df5a
adadezer <adadezer@gmail.com>
2025-07-07 15:43:26 -0300

    feat: creation doctors card

---
+++ doutor-agenda/src/app/(protected)/doctors/_components/doctor-card.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import { CalendarIcon, ClockIcon, DollarSignIcon } from "lucide-react";
+import React from "react";
+
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from "@/components/ui/card";
+import { Dialog, DialogTrigger } from "@/components/ui/dialog";
+import { Separator } from "@/components/ui/separator";
+import { doctorsTable } from "@/db/schema";
+
+import UpsertDoctorForm from "./upsert-doctor-form";
+
+interface DoctorCardProps {
+  doctor: typeof doctorsTable.$inferSelect;
+}
+function DoctorCard({ doctor }: DoctorCardProps) {
+  const doctorInitials = doctor.name
+    .split(" ")
+    .map((name: string) => name[0].toUpperCase())
+    .join("");
+
+  return (
+    <Card>
+      <CardHeader>
+        <div className="flex items-center gap-2">
+          <Avatar className="h-10 w-10">
+            <AvatarFallback>{doctorInitials}</AvatarFallback>
+          </Avatar>
+          <div>
+            <h3 className="text-sm font-medium">{doctor.name}</h3>
+            <p className="text-muted-foreground text-sm">{doctor.specialty}</p>
+          </div>
+        </div>
+      </CardHeader>
+      <Separator />
+      <CardContent className="flex flex-col gap-2">
+        <Badge variant="outline">
+          <CalendarIcon className="mr-1" />
+          Segunda a Sexta
+        </Badge>
+        <Badge variant="outline">
+          <ClockIcon className="mr-1" />
+          {doctor.availableFromTime} - {doctor.availableToTime}
+        </Badge>
+        <Badge variant="outline">
+          <DollarSignIcon className="mr-1" />
+          {doctor.appointmentPriceInCents / 100}
+        </Badge>
+      </CardContent>
+      <Separator />
+      <CardFooter>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button className="w-full">Ver Detalhes</Button>
+          </DialogTrigger>
+          <UpsertDoctorForm />
+        </Dialog>
+      </CardFooter>
+    </Card>
+  );
+}
+
+export default DoctorCard;
