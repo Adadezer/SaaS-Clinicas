1f8848ef2e3c55e3065fbba30b764b104607831f
adadezer <adadezer@gmail.com>
2025-07-10 12:56:08 -0300

    feat: add patient table

---
+++ doutor-agenda/src/app/(protected)/patients/_components/table-columns.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import { EditIcon, MoreVerticalIcon, TrashIcon } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { patientsTable } from "@/db/schema";
+
+type Patient = typeof patientsTable.$inferSelect;
+
+export const patientTableColumns: ColumnDef<Patient>[] = [
+  {
+    id: "name",
+    accessorKey: "name",
+    header: "Nome",
+  },
+  {
+    id: "email",
+    accessorKey: "email",
+    header: "Email",
+  },
+  {
+    id: "phoneNumber",
+    accessorKey: "phoneNumber",
+    header: "Telefone",
+    cell: (params) => {
+      const phoneNumber = params.getValue<string>() || "";
+      const cleaned = phoneNumber.replace(/\D/g, "");
+      if (cleaned.length === 11) {
+        return `(${cleaned.slice(0, 2)}) ${cleaned.slice(2, 7)}-${cleaned.slice(7)}`;
+      }
+      return phoneNumber;
+    },
+  },
+  {
+    id: "sex",
+    accessorKey: "sex",
+    header: "Sexo",
+    cell: (params) => {
+      const patient = params.row.original;
+      return patient.sex === "male" ? "Masculino" : "Feminino";
+    },
+  },
+
+  {
+    id: "actions",
+    cell: (params) => {
+      const patient = params.row.original;
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Button variant="ghost" size="icon">
+              <MoreVerticalIcon className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>{patient.name}</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <EditIcon /> Editar
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <TrashIcon /> Excluir
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
